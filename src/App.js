64926ffff5d44314b923291fb3afd1c06ff6066f
Thomas.dryjard <newwbuiss@gmail.com>
2019-12-19 18:12:36 +0100

    add page select jour, payment, reserved et router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import Vote from "./components/Vote/Vote";
+import {Switch, Route} from 'router-react-dom'
+import SelectDate from './components/SelectDate'
 import './App.css';
 
 function App() {
 
 	return (
 		<div className="App">
-			<Vote />
+      <Switch>
+        <Route exact path ='/'>{SelectDate}</Route>
+        <Route exact path ='/payment'>{Payment}</Route>
+        <Route exact path ='/reservate'>{Reservate}</Route>
+        <Route exact path ='/vote'>{Vote}</Route>
+      </Switch>
 		</div>
 	);
 }
