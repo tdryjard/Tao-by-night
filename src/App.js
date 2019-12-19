d81069949e27c54c33db25fceee7a9bc2f02384c
Thomas.dryjard <newwbuiss@gmail.com>
2019-12-19 15:49:52 +0100

    page selectJour, payment et reservate en cours

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Route, Switch} from 'react-router-dom'
+import SelectDate from './components/SelectDate'
+import Payment from './components/Payment'
+import Reservate from './components/Reservate'
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+        <Route exact path="/">{SelectDate}</Route>
+        <Route path='/payment'>{Payment}</Route>
+        <Route path='/reservate'>{Reservate}</Route>
+      </Switch>
     </div>
   );
 }
