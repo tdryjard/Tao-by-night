a0105d90b061931035561778f3c3d8f8670e2a88
Thomas.dryjard <newwbuiss@gmail.com>
2019-12-20 03:13:36 +0100

    save

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import emailjs from 'emailjs-com';
 
 
 
 export default function ContactUs() {
 
+  const [email, setEmail] = useState('')
+
   function sendEmail(e) {
     e.preventDefault();
 
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID')
+    emailjs.sendForm('gmail', 'tao_by_night', e.target, 'user_JhMvHUyg9uzqFksC7Ppue')
       .then((result) => {
           console.log(result.text);
       }, (error) => {
           console.log(error.text);
       });
+  }
+
+  const add = (event) => {
+      setEmail(event.target.value)
+      console.log(email)
   }
 
   return (
@@ -22,7 +29,7 @@ export default function ContactUs() {
       <label>Name</label>
       <input type="text" name="user_name" />
       <label>Email</label>
-      <input type="email" name="user_email" />
+      <input type="email" name="user_email" value={email} onChange={add} />
       <label>Message</label>
       <textarea name="message" />
       <input type="submit" value="Send" />
--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,20 +1,22 @@
 import React, {useState} from 'react'
 import { Link, useParams } from 'react-router-dom'
+import emailjs from 'emailjs-com';
 import './payment.css'
 
 const Payment = () => {
 
-    const [test, settest] = useState('')
+    const [mailSub, setMailSub] = useState()
 
     function sendEmail(e) {
         e.preventDefault();
     
-        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID')
+        emailjs.sendForm('gmail', 'tao_by_night', e.target, 'user_JhMvHUyg9uzqFksC7Ppue')
           .then((result) => {
               console.log(result.text);
           }, (error) => {
               console.log(error.text);
           });
+          setMailSub(1)
       }
 
     return (
@@ -22,20 +24,30 @@ const Payment = () => {
             <div className="titleJour">
                 <h4 className="textJour">Réservation</h4>
             </div>
+            
             <h4 className="textPay">Si la demande pour la ligne n'atteint pas le minimum requis vous ne serez pas débité</h4>
             <section>
-            <input className="cardNumber" placeholder="numéro de carte"/>
-            <input className="cardNumber1" placeholder="expiration"/>
-            <input className="cardNumber2"placeholder="CVV"/>
+            <input className="cardNumber" placeholder="numéro de carte" required="required"/>
+            <input className="cardNumber1" placeholder="expiration" required="required"/>
+            <input className="cardNumber2"placeholder="CVV" required="required"/>
             </section>
-            <h4 className="textPay2">Restez au courant de l'avancé de votre réservation et recevez votre Qr Code par mail</h4>
-            <input className="enterMail" placeholder="Adresse mail" required="required"></input> 
-            <div className="placeButton">
-            <Link to="/contact"><button className="reserveButton">Réserver</button></Link>
+            
 
+            <form className="contactForm" onSubmit={sendEmail}>
+                <h4 className="textPay2">Restez au courant de l'avancé de votre réservation et recevez votre Qr Code par mail</h4>
+                <input className="enterMail" placeholder="Adresse mail" required="required" type="email" name="user_email" />
+                <input className="addMail" type="submit"/>
+                {mailSub ? 
+                <Link to="Reservate"><input className="reserveButton" value="Réserver" /></Link>
+                :
+                <input className="reserveButton2" value="Valider mail" required="required"/>
+                }
+                
+            </form>
+
+            
 
             </div>
-        </div>
     )
 }
 
